8e8acc4e7ba896fe42ea694ad0d3591c3e737c77
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2022-10-22 08:27:15 +0000

    Guess done, left with leveling up the game stage on correct guess.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let username = prompt("Enter your username");
 function game(range){
-  let computerGuess = function random(1, range) {
+  //let range = prompt("Enter your max range for the guessing game")
+let myGuess = prompt("Enter your guessed number");
+  function random(range) {
   return Math.floor(Math.random() * (range - 1 + 1) + 1);
 }
   
-  
+  return random(range);
 }
-let range = prompt("Enter your max range for the guessing game")
-let myGuess = prompt("Enter your guessed number");
+
+console.log(game(prompt("Enter your max range for the guessing game")));
